a68dd2dc000c05340c11b4361a35c283d214fcab
UliyaSnisar <66842163+UliyaSnisar@users.noreply.github.com>
2020-12-04 21:50:20 +0200

    добавляет исправление в дз 7

--- js/task-02.js
+++ js/task-02.js
@@ -7,28 +7,12 @@ const ingredients = [
   'Приправы',
 ];
 
-const ulRef = document.querySelector('#ingredients');
-
-const li1 = document.createElement('li');
-li1.textContent = 'Картошка';
-
-const li2 = document.createElement('li');
-li2.textContent = 'Грибы';
-
-const li3 = document.createElement('li');
-li3.textContent = 'Чеснок';
-
-const li4 = document.createElement('li');
-li4.textContent = 'Помидоры';
-
-const li5 = document.createElement('li');
-li5.textContent = 'Зелень';
-
-const li6 = document.createElement('li');
-li6.textContent = 'Приправы';
-
-ulRef.append(li1, li2, li3, li4, li5, li6);
-
-console.log(ulRef);
+const ingredientList = document.querySelector('#ingredients');
+const list = ingredients.map(el => {
+  const li = document.createElement('li');
+  li.textContent = el;
+  return li;
+});
+ingredientList.append(...list);
 
 
